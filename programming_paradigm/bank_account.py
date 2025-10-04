fc5e28114dcd5f77de307a5591ee7ddd47366124
Radwa1055 <radwa.r2020@gmail.com>
2025-10-05 00:20:41 +0200

    add bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -20,7 +20,7 @@ class BankAccount:
 
     def display_balance(self):# استعلام عن الرصيد 
         """Display the current account balance."""
-        print(f"Current Balance: ${self.__account_balance}")
+        print(f"Current Balance: ${self.__account_balance:2f}")
 
     #لازم الفايلين يكونو في نفس الفولدرالكبير عشان اشغلهم مع بعض 
         
