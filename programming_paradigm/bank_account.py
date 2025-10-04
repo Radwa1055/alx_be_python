a022f54dfe1fe2d6cd1e397536c4135874d820cc
Radwa1055 <radwa.r2020@gmail.com>
2025-10-05 00:29:57 +0200

    add bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,27 +1,20 @@
-# bank_account.py
-
 class BankAccount:
     def __init__(self, initial_balance=0):
-        """Initialize account with an optional starting balance."""
-        self.__account_balance = initial_balance  # خاصية خاصة (Encapsulation) عشان كا استخدم __ عشان محدش يقدر يير فيها من برة لكلاس 
+        self.__account_balance = initial_balance
 
     def deposit(self, amount):
-        """Add amount to balance."""
         if amount > 0:
-            self.__account_balance += amount  #نفس الحكاية 
+            self.__account_balance += amount
 
     def withdraw(self, amount):
-        """Withdraw amount if funds are sufficient."""
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
             return False
 
-    def display_balance(self):# استعلام عن الرصيد 
-        """Display the current account balance."""
-        print(f"Current Balance: ${self.__account_balance:4f}")
+    def display_balance(self):
+        print("Current Balance:", self.__account_balance)
 
-    #لازم الفايلين يكونو في نفس الفولدرالكبير عشان اشغلهم مع بعض 
-        
+
         
