84e599492ad9785730667dbe593f23064ba8a66e
Radwa1055 <radwa.mah105@gmail.com>
2025-10-11 21:50:30 +0300

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,8 +6,10 @@ class Book:
     def __del__(self) :
       print(f"Deleting {self.title}") 
     
-    def __str__(self):
-        return "(title) by (author), published in (year)"
-    
-    def __rep__(self):
-      return f"Book('{self.title}', '{self.author}', {self.year})"
+   def __str__(self):
+        """User-friendly string representation."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Official representation that can recreate the Book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
