f18873cd7b6b5883f51be59ec2d4f2215b5f04c0
Radwa1055 <radwa.mah105@gmail.com>
2025-10-11 22:31:22 +0300

    Update polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,22 +1,27 @@
 import math
+
 class Shape:
-    def __init__area(self):
+    def area(self):
+        
         raise NotImplementedError("Subclasses must override the area() method.")
 
 
 class Rectangle(Shape):
-    def __init__Rectangle(self, length, width):
-        self.lenght = length
-        self.width = width
+    def __init__(self, length, width):
         
+        self.length = length
+        self.width = width
+
     def area(self):
-        return self.length * self.width    
-    
-    
-    
+      
+        return self.length * self.width
+
+
 class Circle(Shape):
     def __init__(self, radius):
+        
         self.radius = radius
 
     def area(self):
-        return math.pi * (self.radius ** 2) 
+      
+        return math.pi * (self.radius ** 2)
