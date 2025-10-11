8202c561e9a685d156feb344c9cee5199be75490
Radwa1055 <radwa.r2020@gmail.com>
2025-10-11 22:15:50 +0200

    ADD POLYMORPHISM

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+import math
+class Shape:
+    def __init__area(self):
+        raise NotImplementedError("Subclasses must override the area() method.")
+
+
+class Rectangle(Shape):
+    def __init__Rectangle(self, length, width):
+        self.lenght = length
+        self.width = width
+        
+    def area(self):
+        return self.length * self.width    
+    
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2) 
